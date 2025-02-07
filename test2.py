4d76774faa2e8609ac9d0092f3ba17f093db4f9b
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2025-02-07 22:52:39 +0800

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,42 @@
+import time
+from DrissionPage import Chromium, ChromiumOptions
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(1920, 1080))
+display.start()
+
+co = ChromiumOptions().auto_port()
+
+arguments = [
+    "--disable-gpu",
+    "--no-sandbox",
+#    "--window-size=1920,1080",
+    "--disable-dev-shm-usage",
+]
+
+for argument in arguments:
+    co.set_argument(argument)
+
+browser = Chromium(co)
+
+tab = browser.latest_tab
+
+# 访问网页
+tab.get('https://github.com/trending')
+print(tab.title)
+
+ele = tab.ele('x:/html/body/div[1]/div[4]/main/div[1]/nav/div/a[1]')
+print(ele.text)
+
+ele.click()
+
+time.sleep(2)
+try:
+	ele_for_check = tab.ele('x:/html/body/div[1]/div[4]/main/div[2]/div/div/div[2]/h2')
+	print(ele_for_check.text)
+except:
+	print('元素丢失')
+
+
+browser.quit()
+display.stop()
