81109a68b71ea5612b1fcec7513f432eb8533a57
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2025-02-08 18:33:04 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+sudo which google-chrome
 
-which google-chrome
+sudo find / -name google-chrome
+
+locate google-chrome
+
+sudo updatedb
+locate google-chrome
+
+ls -l /usr/bin/google-chrome
