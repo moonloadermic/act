51b0fd315d5c93b142dcb5c00096ff6a483df19d
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2025-02-08 22:37:22 +0800

    Update test-cf.py

--- test-cf.py
+++ test-cf.py
@@ -3,9 +3,9 @@ import time
 from DrissionPage import Chromium, ChromiumOptions
 from pyvirtualdisplay import Display
 
-url = 'https://nopecha.com/demo/cloudflare'
-ele_for_check_path = 'x://*[@id="padded_content"]/div/h3'
-ele_for_check_text = 'CAPTCHA Demo'
+url = 'https://test.aiuuo.com/'
+ele_for_check_path = 'x:/html/body/pre'
+ele_for_check_text = 'Hello World!'
 
 display = Display(visible=0, size=(1920, 1080))
 display.start()
@@ -16,7 +16,7 @@ arguments = [
     "--disable-gpu",
     "--no-sandbox",
     "--disable-dev-shm-usage",
-    # "--window-size=1920,1080"
+#    "--window-size=1920,1080",    
 ]
 
 for argument in arguments:
@@ -38,15 +38,24 @@ for i in range(2):
         tab.wait.load_start()
         time.sleep(2)
         ele = tab.ele("@name=cf-turnstile-response").parent()
-        print(ele)
-        loc = ele.rect.screen_location
+        # 找到复选框并返回元素中点在屏幕的位置
+        checkbox = ele.sr('t:iframe')('t:body').sr('t:input')
+        loc = checkbox.rect.screen_midpoint
+        # 找到'Verify you are human'并返回元素中点在屏幕的位置
+        # ele_Confirmhuman = checkbox.next(2)
+        # print(ele_Confirmhuman.text)
+        # loc = ele_Confirmhuman.rect.screen_midpoint
         print(loc)
-        x=loc[0]+32
-        y=loc[1]+35
-        print(x,y)
+        x = loc[0]
+        y = loc[1]
+        print(x,y)	
         time.sleep(2)
         command = ['xdotool', 'mousemove', str(x), str(y), 'click', '1']
-        subprocess.run(command)	
+        subprocess.run(command)
+        for i in range(5):
+            tab.get_screenshot(name='pic{i+1}.jpg')
+            time.sleep(0.5)
+
         try:
             time.sleep(5)
             ele_for_check = tab.ele(ele_for_check_path)
