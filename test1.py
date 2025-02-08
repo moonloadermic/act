6eda9e58eb916d9a0a6689c0a26abec72f445e19
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2025-02-08 21:48:48 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,9 @@
 import time
 from DrissionPage import Chromium, ChromiumOptions
-from pyvirtualdisplay import Display
+#from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(1920, 1080))
-display.start()
+#display = Display(visible=0, size=(1920, 1080))
+#display.start()
 
 co = ChromiumOptions().auto_port()
 
@@ -35,8 +35,6 @@ print(ele.text)
 
 ele.click()
 
-print(tab.title)
-
 try:
 	ele_for_check = tab.ele('x:/html/body/div[1]/div[4]/main/div[2]/div/div/div[2]/h2')
 	print(ele_for_check.text)
@@ -45,4 +43,4 @@ except:
 
 
 browser.quit()
-display.stop()
+#display.stop()
