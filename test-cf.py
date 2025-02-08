3cce73235fa3f400f6bdc3e0ab8ffcddca79b21e
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2025-02-08 19:38:19 +0800

    Update test-cf.py

--- test-cf.py
+++ test-cf.py
@@ -7,6 +7,9 @@ url = 'https://test.aiuuo.com/'
 ele_for_check_path = 'x:/html/body/pre'
 ele_for_check_text = 'Hello World!'
 
+display = Display(visible=0, size=(1920, 1080))
+display.start()
+
 co = ChromiumOptions().auto_port()
 
 arguments = [
@@ -37,7 +40,14 @@ for i in range(2):
         ele = tab.ele("@name=cf-turnstile-response").parent()
         # 找到复选框并返回元素中点在屏幕的位置
         checkbox = ele.sr('t:iframe')('t:body').sr('t:input')
-        checkbox.click()
+        # loc = checkbox.rect.screen_midpoint
+        # 找到'Verify you are human'并返回元素中点在屏幕的位置
+        ele_Confirmhuman = checkbox.next(2)
+        print(ele_Confirmhuman.text)
+        loc = ele_Confirmhuman.rect.screen_midpoint
+        print(loc)
+        time.sleep(2)
+        ele.click(by_js=True)
         try:
             time.sleep(5)
             ele_for_check = tab.ele(ele_for_check_path)
@@ -57,4 +67,4 @@ if not bypass:
 
 time.sleep(5)
 browser.quit()
-
+display.stop()
