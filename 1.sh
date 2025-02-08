eefe8888ea4b1017b6052dc8206400056a68dec7
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2025-02-08 18:46:46 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,11 +1,4 @@
-chrome_path=$(which google-chrome)
-
-# 如果找到了路径，输出路径；否则输出未找到的消息
-if [ -n "$chrome_path" ]; then
-    echo "Google Chrome 安装路径: $chrome_path"
-else
-    echo "未找到 Google Chrome 的安装路径"
-fi
+which google-chrome
 
 sudo find / -name google-chrome
 
